03a1e1bb907be37c617261e7ab3505e16d0c0d56
Dolores Perez <dperez@asap-consulting.com.ar>
2022-01-06 21:08:58 -0300

    wip: se agregan funciones para pokemon prev y pokemon siguiente con los botones

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,19 +9,19 @@ import { ProductosService } from '../../services/productos.service';
 export class HomeComponent implements OnInit {
 
   pokemones: any = [];
-  pokemonID: string = '';
+  pokemonID: number = 1;
   // pokemonID: number = 72;
   pokemonData: any;
   imageSpinner: boolean = true;
   pokemonTypeOne: string = '';
   pokemonTypeTwo: string = '';
-  booleanooo: boolean = false;
-
+  prev: any; 
+  next: any;
 
   constructor(private productosSrv: ProductosService) {}
 
   ngOnInit(): void {
-    console.log(this.pokemonID);
+    this.getRandomPokemon();
   }
 
   private getTodosLosPokemones(): void {
@@ -38,8 +38,8 @@ export class HomeComponent implements OnInit {
     );
   }
 
-  public getPokemonesById(): void {
-    this.pokemonID = Math.floor(Math.random() * 150 + 1).toString();
+  public getRandomPokemon(): void {
+    this.pokemonID = Math.floor(Math.random() * 150 + 1);
     this.productosSrv
       .getPokemonesById(this.pokemonID)
       .subscribe((data: any) => {
@@ -53,7 +53,6 @@ export class HomeComponent implements OnInit {
 
   private getPokemonTypes(): void {
     this.pokemonTypeOne = this.pokemonData.types[0].type.name;
-    console.log("tipo 1", this.pokemonTypeOne)
     if(this.pokemonData.types[1]){
       this.pokemonTypeTwo = this.pokemonData.types[1].type.name;
     }
@@ -64,4 +63,30 @@ export class HomeComponent implements OnInit {
     // }
   }
 
+
+  public previuosPokemon(): void {
+    this.prev = this.pokemonID --;
+    this.productosSrv.getPokemonesById(this.pokemonID).subscribe((data: any)=>{
+      this.pokemonData = data;
+      console.log(this.pokemonData);
+      this.getPokemonTypes();
+      this.pokemones.push(this.pokemonData);
+    })
+    this.pokemones = [];
+    console.log('prev', this.prev)
+  }
+  
+  public nextPokemon(): void{
+    this.next = this.pokemonID++;
+    this.productosSrv.getPokemonesById(this.pokemonID).subscribe((data: any)=>{
+      this.pokemonData = data;
+      this.getPokemonTypes();
+      this.pokemones.push(this.pokemonData);
+    })
+    this.pokemones = [];
+  }
+
 }
+
+
+// TODO: EL LLAMADO AL SERVICE JUNTARLO EN UNA FUNCION
